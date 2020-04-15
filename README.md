f914020eb0ed25e4e2e09a014fbe071358ae96cc
lg920810 <lg920810@163.com>
2020-04-15 18:59:28 +0800

    add README.md

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# image-segmentation-keras
+﻿#####image-segmentation-keras
+
+
+###
+安装需要的包
+例如 pip install keras(cv2,argparse,PIL,skimage,numpy,matplotlib,os,pandas等)
+
+
+###
+语义分割模型
+在model文件夹中
+——deeplab v3
+——unet
+
+###
+损失函数和度量指标
+在losses.py文件中
+包含了iou loss，softmax dice loss，weighted loss，jaccard，f1score，focal_loss
+dice for binary，weighted_bce_dice_loss，BINARY LOSSES，MULTICLASS LOSSES
+precision，recall
+
+
+###
+训练
+运行 python train.py (若要修改参数，在train.py修改即可)
+或不在train.py修改参数，运行例如python train.py --image_size --batch_size
+
+###
+测试
+运行python inference.py
--- train.py
+++ train.py
@@ -9,8 +9,6 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.Session(config=config)
 import argparse
-import pandas as pd
-import numpy as np
 from keras.utils import multi_gpu_model
 import keras.optimizers as optim
 from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, TensorBoard, CSVLogger
@@ -19,39 +17,38 @@ from dataloader import DataGenerator
 from losses import dice_coeff, bce_dice_loss, keras_lovasz_hinge
 from models.deeplabv3 import Deeplabv3
 from models.unet import UNet
-from models.danet import DANet
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("model", type=str, default='deeplabv3+', choices=['unet', 'deeplabv3+', 'danet'],
+    parser.add_argument("model", type=str, choices=['unet', 'deeplabv3+', 'fpn-vnet'],
                         help="Select the model to use training")
     parser.add_argument("--image_size", type=int, default=256,
-                        help="display a square of a given number")
+                        help="Size of image")
     parser.add_argument("--image_depth", type=int, default=3,
-                        help="display a square of a given number")
+                        help="Depth of image")
     parser.add_argument("--num_classes", type=int, default=3,
-                        help="display a square of a given number")
+                        help="The number of classes to be segmented")
     parser.add_argument("--batch_size", type=int, default=4,
-                        help="display a square of a given number")
+                        help="Number of examples per batch")
     parser.add_argument("--lr", type=float, default=1e-3,
-                        help="display a square of a given number")
+                        help="learning rate for the optimizer")
     parser.add_argument("--epochs", type=int, default=50,
-                        help="display a square of a given number")
+                        help="The number of epochs to train for")
     parser.add_argument("--gpus", type=int, default=1,
-                        help="display a square of a given number")
+                        help="GPUs ID")
     parser.add_argument("--dataset", type=str, default=os.curdir,
-                        help="display a square of a given number")
+                        help="Path to segmentation dataset")
     parser.add_argument("--outputs", type=str, default='outputs',
-                        help="display a square of a given number")
+                        help="Training output location")
     parser.add_argument("--mode", type=str, default='train', choices=['train', 'validation'],
-                        help="display a square of a given number")
+                        help="Train or verification")
     parser.add_argument("--weights", type=str, default=None,
-                        help="display a square of a given number")
+                        help="The path of weights to be loaded")
     parser.add_argument("--loss", type=str, default='bce_dice_loss', choices=['bce_dice_loss', 'dice_loss', 'bce_loss'],
-                        help="display a square of a given number")
+                        help="The loss function for traing")
     parser.add_argument("--comparison", type=bool, default=False,
-                        help="display a square of a given number")
+                        help="comparison of integers of different signs")
 
     args = parser.parse_args()
     args.outputs = args.outputs + '_' + args.model
@@ -60,17 +57,16 @@ if __name__ == '__main__':
 
     # instantiate model
     if args.model == 'deeplabv3+':
-        model = Deeplabv3(input_shape=(args.image_size, args.image_size, args.image_depth),
+        model = Deeplabv3(input_shape=[args.image_size, args.image_size, args.image_depth],
                           classes=args.num_classes,
                           backbone='mobilenetv2',
                           OS=16,
                           alpha=1.)
     elif args.model == 'unet':
-        model = UNet(input_size=(args.image_size, args.image_size, args.image_depth),
+        model = UNet(input_size=[args.image_size, args.image_size, args.image_depth],
                      classes=args.num_classes)
     else:
-        model = DANet(input_size=(args.image_size, args.image_size, args.image_depth),
-                      classes=args.num_classes)
+        pass
 
     model.summary()
 
@@ -114,7 +110,7 @@ if __name__ == '__main__':
                                   write_graph=True,
                                   write_images=True)
 
-        callbacks = [lr, ckpt, tensorboard, csv_log]
+        callbacks = [lr, ckpt, tensorboard]
 
         train_generator = DataGenerator(path=os.path.join(args.dataset, 'train'),
                                         batch_size=args.batch_size)
@@ -135,10 +131,3 @@ if __name__ == '__main__':
                             )
 
         model.save(os.path.join(args.outputs, 'best_model_final.h5'))
-
-        train_csv = pd.read_csv(os.path.join(args.outputs, 'logger_model_train.csv'))
-
-        val_dice = train_csv['val_dice_coeff']
-        best_val_dice = np.round(np.max(val_dice), 2)
-        val_epoch = np.where(val_dice == np.max(val_dice))
-        print(args.model, ': The best dice value in validation is', best_val_dice, 'on epoch', val_epoch[0][0])
